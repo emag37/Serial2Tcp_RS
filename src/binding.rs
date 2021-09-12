4cb96475bb546b0cff22afb4380100231ec50d7d
Eric M <turbosuper37@hotmail.com>
2021-09-11 22:27:43 -0400

    Restart on connection failure. Logs are prefixed with binding type.

--- src/binding.rs
+++ src/binding.rs
@@ -5,6 +5,7 @@ use serialport::{SerialPort};
 use std::net::{TcpStream};
 use std::io::{Read,Write};
 use std::option::Option;
+use std::fmt;
 
 #[derive(Default, Clone)]
 pub struct BindingConfig {
@@ -26,101 +27,123 @@ impl Drop for ActiveBinding {
     }
 }
 
-fn start_tcp_read_thread(config: &BindingConfig, active_flag: Arc<AtomicBool>, com: serialport::COMPort, tcp_listener: std::net::TcpListener) -> std::thread::JoinHandle<()> {
+impl fmt::Display for BindingConfig {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({} <-> {})", self.tcp_host, self.com_port)
+    }
+}
+
+fn start_tcp_read_thread(config: &BindingConfig, active_flag: Arc<AtomicBool>, tcp_listener: std::net::TcpListener) -> std::thread::JoinHandle<()> {
     let config = config.clone();
+
     std::thread::spawn(move || {
-        println!("Waiting for a connection on {}", config.tcp_host);
-        let (tcp_stream_write, _) = match tcp_listener.accept() {
-            Ok(stream) => stream,
-            Err(err) => {
-                println!("Accepting connection on: {}: {}", config.tcp_host, err);
-                active_flag.store(false, std::sync::atomic::Ordering::Release);
-                return
-            }
-        };
-        
-        println!("Got a connection, beginning relay {} <-> {}", config.tcp_host, config.com_port);
-        let tcp_stream_read =  match tcp_stream_write.try_clone() {
-            Ok(stream) => stream,
-            Err(err) => {
-                println!("Error cloning TCP stream for read: {}", err);
-                active_flag.store(false, std::sync::atomic::Ordering::Release);
-                return
-            }
-        };
-        
-        let tcp_write_thread = start_tcp_write_thread(&config, active_flag.clone(), com.try_clone_native().unwrap(), tcp_stream_write);
-
-        let mut buf : [u8;1500] = [0;1500];
-        
-        let mut tcp_stream_read = tcp_stream_read;
-        let mut com_write = com;
-
         while active_flag.load(std::sync::atomic::Ordering::Acquire) {
-            let n_read = match tcp_stream_read.read(&mut buf) {
-                Ok(n) => n,
+            let mut com = match serialport::new(&config.com_port, config.baud_rate).open_native() {
+                Ok(port) => port,
                 Err(err) => {
-                    println!("Error reading from TCP stream @ {}: {}", config.tcp_host, err);
-                    break
+                    println!("{}: Failed to open com port: {}, retry in 2 seconds...", config, err);
+                    std::thread::sleep(std::time::Duration::from_secs(2));
+                    continue
                 },
             };
-            if n_read > 0 {
-                match com_write.write(&mut buf[..n_read]) {
-                    Ok(_) => {},
+            let _ = com.set_timeout(std::time::Duration::from_secs(2));
+        
+            println!("{}: Waiting for a connection", config);
+            let (tcp_stream_write, _) = match tcp_listener.accept() {
+                Ok(stream) => stream,
+                Err(err) => {
+                    println!("{}: Error accepting TCP connection: {}", config, err);
+                    continue
+                }
+            };
+            
+            println!("{}: Got a connection, beginning relay.", config);
+            let tcp_stream_read =  match tcp_stream_write.try_clone() {
+                Ok(stream) => stream,
+                Err(err) => {
+                    println!("Error cloning TCP stream for read: {}", err);
+                    continue
+                }
+            };
+            
+            let writer_active_flag = Arc::new(AtomicBool::new(true));
+            let tcp_write_thread = start_tcp_write_thread(&config, writer_active_flag.clone(), com.try_clone_native().unwrap(), tcp_stream_write);
+
+            let mut buf : [u8;1500] = [0;1500];
+            
+            let mut tcp_stream_read = tcp_stream_read;
+            let mut com_write = com.try_clone_native().unwrap();
+
+            loop {
+                let n_read = match tcp_stream_read.read(&mut buf) {
+                    Ok(n) => n,
                     Err(err) => {
-                        println!("Error writing to COM port {}: {}", config.com_port, err);
+                        println!("{}: Error reading from TCP stream: {}", config, err);
                         break
                     },
+                };
+                if n_read > 0 {
+                    match com_write.write(&mut buf[..n_read]) {
+                        Ok(_) => {},
+                        Err(err) => {
+                            println!("{}: Error writing to COM port: {}", config, err);
+                            break
+                        },
+                    }
                 }
             }
+            std::mem::drop(com);
+            std::mem::drop(tcp_stream_read);
+            writer_active_flag.store(false, std::sync::atomic::Ordering::Release);
+            let _ = tcp_write_thread.join();
         }
-        println!("relay {} <-> {} exited", config.tcp_host, config.com_port);
-        active_flag.store(false, std::sync::atomic::Ordering::Release);
-        let _ = tcp_write_thread.join();
+        
+        println!("{}: exited", config);
+        active_flag.store(false, std::sync::atomic::Ordering::Release);   
     })
 }
 
 fn start_tcp_write_thread(config: &BindingConfig, active_flag: Arc<AtomicBool>, com: serialport::COMPort, tcp: TcpStream) -> std::thread::JoinHandle<()> {
     let config_for_tcp_writer = config.clone();
-    let tcp_writer_flag_ref = active_flag.clone();
     std::thread::spawn(move || {
         let mut buf : [u8;1500] = [0;1500];
 
         let mut com_read = com;
         let mut tcp_stream_write = tcp;
 
-        while tcp_writer_flag_ref.load(std::sync::atomic::Ordering::Acquire) {
+        while active_flag.load(std::sync::atomic::Ordering::Acquire) {
             let n_read = match com_read.read(&mut buf){
                 Ok(n_bytes) => n_bytes,
-                Err(ec) => {println!("Error reading from port {}! {}", config_for_tcp_writer.com_port, ec); break}
+                Err(ec) => {
+                    if ec.kind() != std::io::ErrorKind::TimedOut {
+                        println!("{}: Error reading from port: {}", config_for_tcp_writer, ec);
+                    }
+                    0
+                }
             };
-            match tcp_stream_write.write(&mut buf[..n_read]) {
-                Ok(_) => {},
-                Err(err) => {
-                    println!("Error writing from host {}: {}", config_for_tcp_writer.tcp_host, err);
+
+            if n_read > 0 {
+                match tcp_stream_write.write(&mut buf[..n_read]) {
+                    Ok(_) => {},
+                    Err(err) => {
+                        println!("{}: Error writing from host: {}", config_for_tcp_writer, err);
+                    }
                 }
             }
         }
-        active_flag.store(false, std::sync::atomic::Ordering::Release);
     })
 }
 pub fn start_workers(config: &BindingConfig) -> Result<ActiveBinding, serialport::Error> {
-    println!("Starting relay {} <-> {}", config.tcp_host, config.com_port);
+    println!("{}: Starting relay", config);
 
     let active_flag = Arc::new(AtomicBool::new(true));
-
-    let mut com_read = match serialport::new(&config.com_port, config.baud_rate).open_native() {
-        Ok(port) => port,
-        Err(err) => return Err(err),
-    };
-    let _ = com_read.set_timeout(std::time::Duration::from_secs(2));
 
     let tcp_listener = match std::net::TcpListener::bind(&config.tcp_host) {
         Ok(listener) => listener,
         Err(err) => return Err(serialport::Error::from(err))
     };
 
-    let tcp_read_thread = start_tcp_read_thread(config, active_flag.clone(), com_read, tcp_listener);
+    let tcp_read_thread = start_tcp_read_thread(config, active_flag.clone(), tcp_listener);
 
     Ok(ActiveBinding{ 
         worker: Option::from(tcp_read_thread),
